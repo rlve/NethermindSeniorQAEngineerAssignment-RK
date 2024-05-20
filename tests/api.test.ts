145e7869f6783adab20b5df42a8e211c4430f609
rlve <radioslav@gmail.com>
2024-05-20 16:10:50 +0100

    add basic scenarios

---
+++ tests/api.test.ts
@@ -0,0 +1,30 @@
+import { isHex } from '../lib/helpers';
+import {
+  sendEthBlockNumber,
+  sendEthGetBlockByNumber,
+  sendEthChainId,
+} from '../lib/requests';
+
+describe('eth_blockNumber', () => {
+  it('should return current block number', async () => {
+    const result = await sendEthBlockNumber();
+
+    expect(isHex(result)).toBe(true);
+  });
+});
+
+describe('eth_getBlockByNumber', () => {
+  it('should return block by number', async () => {
+    const result = await sendEthGetBlockByNumber('0x967111', false);
+
+    expect(result).toHaveProperty('author');
+  });
+});
+
+describe('eth_chainId', () => {
+  it('should return ChainID', async () => {
+    const result = await sendEthChainId();
+
+    expect(isHex(result)).toBe(true);
+  });
+});
--- tests/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { sendEthBlockNumber } from "../lib/requests";
-
-describe("test add function", () => {
-  it("should return 15 for add(10,5)", async () => {
-    await sendEthBlockNumber();
-  });
-});
