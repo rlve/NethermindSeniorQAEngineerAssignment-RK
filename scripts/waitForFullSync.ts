edf748a126a80a979132859ef246c9482982bca9
rlve <radioslav@gmail.com>
2024-05-21 08:11:28 +0100

    test

--- scripts/waitForFullSync.ts
+++ scripts/waitForFullSync.ts
@@ -3,25 +3,26 @@ import { sendDebugGetSyncStage, sendEthSyncing } from '../lib/requests';
 import { retry } from 'ts-retry-promise';
 
 (async () => {
-  console.log(await sendDebugGetSyncStage());
   await retry(
     async () => {
-      const syncStateResult = await sendDebugGetSyncStage();
+      await retry(
+        async () => {
+          const syncStateResult = await sendDebugGetSyncStage();
 
-      console.log(syncStateResult);
+          console.log(syncStateResult);
 
-      if (
-        !syncStateResult.currentStage.includes('StateNodes') &&
-        !syncStateResult.currentStage.includes('WaitingForBlock')
-      ) {
-        throw Error();
-      }
-    },
-    { retries: 'INFINITELY', delay: 5000, timeout: 60000 },
-  );
+          if (
+            !syncStateResult.currentStage.includes('StateNodes') &&
+            !syncStateResult.currentStage.includes('BeaconHeaders') &&
+            !syncStateResult.currentStage.includes('Full') &&
+            !syncStateResult.currentStage.includes('WaitingForBlock')
+          ) {
+            throw Error();
+          }
+        },
+        { retries: 'INFINITELY', delay: 5000, timeout: 60000 },
+      );
 
-  await retry(
-    async () => {
       const syncingResult = await sendEthSyncing();
 
       console.log(syncingResult);
