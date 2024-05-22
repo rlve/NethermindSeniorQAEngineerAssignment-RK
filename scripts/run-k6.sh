0a4292976cb8baf2cf6cd3b0022ccd88ab2c7f34
rlve <radioslav@gmail.com>
2024-05-22 16:31:34 +0100

    test another jest reporter

--- scripts/run-k6.sh
+++ scripts/run-k6.sh
@@ -4,20 +4,26 @@ npx tsc
 
 export K6_ITERATIONS=1000
 export K6_VUS=10
-./k6 run ./dist/k6-tests/load.js > k6-results.txt  
+echo "Scenario #1 - K6_ITERATIONS=$K6_ITERATIONS K6_VUS=$K6_VUS" > k6-results.txt  
+./k6 run ./dist/k6-tests/load.js >> k6-results.txt  
 
 export K6_VUS=50
+echo "Scenario #2 - K6_ITERATIONS=$K6_ITERATIONS K6_VUS=$K6_VUS" >> k6-results.txt  
 ./k6 run ./dist/k6-tests/load.js >> k6-results.txt  
 
 export K6_VUS=100
+echo "Scenario #3 - K6_ITERATIONS=$K6_ITERATIONS K6_VUS=$K6_VUS" >> k6-results.txt  
 ./k6 run ./dist/k6-tests/load.js >> k6-results.txt  
 
 export K6_ITERATIONS=10000
 export K6_VUS=10
+echo "Scenario #4 - K6_ITERATIONS=$K6_ITERATIONS K6_VUS=$K6_VUS" >> k6-results.txt  
 ./k6 run ./dist/k6-tests/load.js >> k6-results.txt  
 
 export K6_VUS=50
+echo "Scenario #5 - K6_ITERATIONS=$K6_ITERATIONS K6_VUS=$K6_VUS" >> k6-results.txt  
 ./k6 run ./dist/k6-tests/load.js >> k6-results.txt  
 
 export K6_VUS=100
+echo "Scenario #6 - K6_ITERATIONS=$K6_ITERATIONS K6_VUS=$K6_VUS" >> k6-results.txt  
 ./k6 run ./dist/k6-tests/load.js >> k6-results.txt  
