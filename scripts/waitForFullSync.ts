455009b30a525dfba934b2621f34ac0c481dfa63
rlve <radioslav@gmail.com>
2024-05-22 11:55:00 +0100

    try k6 in GH

--- scripts/waitForFullSync.ts
+++ scripts/waitForFullSync.ts
@@ -30,6 +30,6 @@ import { retry } from 'ts-retry-promise';
         throw Error();
       }
     },
-    { retries: 'INFINITELY', delay: 5000, timeout: 30 * 60000 },
+    { retries: 'INFINITELY', delay: 5000, timeout: 20 * 60000 },
   );
 })();
