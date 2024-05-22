7c5b77690c33b12f13fad363379340326dfce4f5
rlve <radioslav@gmail.com>
2024-05-22 13:19:11 +0100

    test

--- scripts/run-k6.sh
+++ scripts/run-k6.sh
@@ -4,20 +4,20 @@ npx tsc
 
 export K6_ITERATIONS=1000
 export K6_VUS=10
-k6 run ./dist/k6/load.js > k6-results.txt  
+k6 run ./dist/k6-tests/load.js > k6-results.txt  
 
 export K6_VUS=50
-k6 run ./dist/k6/load.js >> k6-results.txt  
+k6 run ./dist/k6-tests/load.js >> k6-results.txt  
 
 export K6_VUS=100
-k6 run ./dist/k6/load.js >> k6-results.txt  
+k6 run ./dist/k6-tests/load.js >> k6-results.txt  
 
 export K6_ITERATIONS=10000
 export K6_VUS=10
-k6 run ./dist/k6/load.js >> k6-results.txt  
+k6 run ./dist/k6-tests/load.js >> k6-results.txt  
 
 export K6_VUS=50
-k6 run ./dist/k6/load.js >> k6-results.txt  
+k6 run ./dist/k6-tests/load.js >> k6-results.txt  
 
 export K6_VUS=100
-k6 run ./dist/k6/load.js >> k6-results.txt  
+k6 run ./dist/k6-tests/load.js >> k6-results.txt  
