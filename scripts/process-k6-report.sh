9b4ff56992c4a59e2a5f8de4cdb44e1ffde0ddb1
rlve <radioslav@gmail.com>
2024-05-22 17:00:06 +0100

    fix

--- scripts/process-k6-report.sh
+++ scripts/process-k6-report.sh
@@ -2,4 +2,12 @@
 
 input_file="./k6-results.txt"
 
-awk '/^ *Scenario|^ *checks|^ *data_received|^ *data_sent|^ *http_req_blocked|^ *http_req_connecting|^ *http_req_duration|^ *http_req_failed|^ *http_req_receiving|^ *http_req_sending|^ *http_req_tls_handshaking|^ *http_req_waiting|^ *http_reqs|^ *iteration_duration|^ *iterations/ {print}' "$input_file" >> $GITHUB_STEP_SUMMARY
+
+K6_SUMMARY=$(awk '/^ *Scenario|^ *checks|^ *data_received|^ *data_sent|^ *http_req_blocked|^ *http_req_connecting|^ *http_req_duration|^ *http_req_failed|^ *http_req_receiving|^ *http_req_sending|^ *http_req_tls_handshaking|^ *http_req_waiting|^ *http_reqs|^ *iteration_duration|^ *iterations/ {print}' "$input_file")
+
+printf "%s\n" "
+# Performance Tests Report
+\`\`\`
+$K6_SUMMARY
+\`\`\`
+" >> $GITHUB_STEP_SUMMARY
