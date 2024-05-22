a4c8b669267501d2ba845f7a051fc219ade158e6
rlve <radioslav@gmail.com>
2024-05-22 15:28:51 +0100

    validate block schema

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,9 +1,9 @@
-import { isHex } from '../lib/helpers';
+import { hasValidSchema, isHex } from './lib/assertions';
 import {
   sendEthBlockNumber,
   sendEthGetBlockByNumber,
   sendEthChainId,
-} from '../lib/requests';
+} from './lib/requests';
 
 describe('eth_blockNumber', () => {
   it('should return current block number', async () => {
@@ -14,10 +14,10 @@ describe('eth_blockNumber', () => {
 });
 
 describe('eth_getBlockByNumber', () => {
-  it('should return block by number', async () => {
+  it('should return block by number with a valid schema', async () => {
     const result = await sendEthGetBlockByNumber('latest', false);
 
-    expect(result).toHaveProperty('author');
+    expect(hasValidSchema(result)).toBe(true);
   });
 });
 
