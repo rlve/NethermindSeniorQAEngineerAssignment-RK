c92ed8124fd0411409c30b125cb8e52fbd129aa0
rlve <radioslav@gmail.com>
2024-05-23 12:31:33 +0100

    refactor waitForFullSync

--- scripts/waitForFullSync.ts
+++ scripts/waitForFullSync.ts
@@ -1,35 +1,43 @@
+import { retry } from 'ts-retry-promise';
 import { sendDebugGetSyncStage, sendEthSyncing } from '../tests/lib/requests';
 
-import { retry } from 'ts-retry-promise';
+async function waitForSyncStage() {
+  const syncStateResult = await sendDebugGetSyncStage();
+
+  console.log(syncStateResult);
+
+  if (
+    !syncStateResult.currentStage.includes('StateNodes') &&
+    !syncStateResult.currentStage.includes('Full') &&
+    !syncStateResult.currentStage.includes('WaitingForBlock')
+  ) {
+    throw Error();
+  }
+}
+
+async function waitForEthSyncing() {
+  const syncingResult = await sendEthSyncing();
+
+  console.log(syncingResult);
+
+  if (syncingResult) {
+    throw Error();
+  }
+}
+
+const DELAY = 5000;
 
 (async () => {
   await retry(
     async () => {
-      await retry(
-        async () => {
-          const syncStateResult = await sendDebugGetSyncStage();
+      await retry(async () => waitForSyncStage(), {
+        retries: 'INFINITELY',
+        delay: DELAY,
+        timeout: 60 * 1000,
+      });
 
-          console.log(syncStateResult);
-
-          if (
-            !syncStateResult.currentStage.includes('StateNodes') &&
-            !syncStateResult.currentStage.includes('Full') &&
-            !syncStateResult.currentStage.includes('WaitingForBlock')
-          ) {
-            throw Error();
-          }
-        },
-        { retries: 'INFINITELY', delay: 5000, timeout: 60000 },
-      );
-
-      const syncingResult = await sendEthSyncing();
-
-      console.log(syncingResult);
-
-      if (syncingResult) {
-        throw Error();
-      }
+      await waitForEthSyncing();
     },
-    { retries: 'INFINITELY', delay: 5000, timeout: 20 * 60000 },
+    { retries: 'INFINITELY', delay: DELAY, timeout: 20 * 60000 },
   );
 })();
